aa79bc53ec77a01d9f20051e63798228c58584ce
Vivi <havntme@gmail.com>
2025-06-18 16:36:43 -0400

    added logo art

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@
 use sysinfo::{
     Disks, System
 };
-
-const TAB: &str = "\t\t\t\t"; //tabs for temporary spacing
 const CLR: &str = "\x1b[0m"; //Clear color
 const RED: &str = "\x1b[31m"; //RED
 const BLUE: &str = "\x1b[36m"; //BLUE
@@ -11,15 +9,22 @@ const YELLOW: &str = "\x1b[33m"; //YELLOW
 const GREEN: &str = "\x1b[32m"; //GREEN
 //
 fn main() {
+    let logo1: String = String::from(r"-------------------");
+    let logo2: String = String::from(r"------|\___/|------");
+    let logo3: String = String::from(r"---n--/ O  O\------");
+    let logo4: String = String::from(r"---\\ | =( w )=----");
+    let logo5: String = String::from(r"----\/       \-----");
+    let logo6: String = String::from(r"-----(|||) (|||)---");
+    let logo7: String = String::from("-----\x1b[31mr \x1b[36mF e t c h\x1b[0m---");
     let mut sys = System::new_all();
-
+ 
     sys.refresh_all();
 
-    println!("\n{RED}{TAB}r{BLUE}Fetch{CLR} - created by vivi\n{BLUE}------------------------------------------------------------------------------{CLR}");
+    println!("\n{RED}\t\t\tr{BLUE}Fetch{CLR} - created by vivi\n{BLUE}---------------------------------------------------------------{CLR}");
 
-    println!("{TAB}{YELLOW}Welcome{RED}    {}!{CLR}", whoami::username());
-    println!("{TAB}{YELLOW}System OS:{CLR}{:?}", System::long_os_version().unwrap());
-    println!("{TAB}{YELLOW}Kernel:{CLR}   {:?}", System::kernel_version().unwrap());
+    println!("{logo1}{YELLOW}Welcome{RED}    {}@{}!{CLR}", whoami::username(), System::host_name().unwrap());
+    println!("{logo1}{YELLOW}System OS:{CLR} {} {}", System::distribution_id(), System::cpu_arch());
+    println!("{logo2}{YELLOW}Kernel:{CLR}    {:?}", System::kernel_version().unwrap());
     
     //uptime stuff- doing basic conversions from seconds to minuts/hours and modifying output text
     //accordingly
@@ -38,22 +43,22 @@ fn main() {
     } else {
         time = format!("{RED}err{CLR}")
     }
-    println!("{TAB}{YELLOW}Uptime:{CLR}    {time}");
+    println!("{logo3}{YELLOW}Uptime:{CLR}    {time}");
 
     //Memory
-    println!("{TAB}{BLUE}==> Memory <=={CLR}");
+    println!("{logo4}{BLUE}==> Memory <=={CLR}");
     let mem_used = sys.used_memory() / 1000000;
     let mem_total = sys.total_memory() / 1000000;
-    println!("{TAB}{BLUE}Used:{CLR}      {}/{} MB", mem_used, mem_total);
+    println!("{logo5}{BLUE}Used:{CLR}      {}/{} MB", mem_used, mem_total);
 
     //CPU
-    println!("{TAB}{YELLOW}==> CPU <=={CLR}");
-    println!("{TAB}{YELLOW}CPU: {CLR}{}", sys.cpus()[0].brand());
+    println!("{logo6}{YELLOW}==> CPU <=={CLR}");
+    println!("{logo7}{YELLOW}CPU: {CLR}{}", sys.cpus()[0].brand());
     //println!("{TAB}{YELLOW}{}{CLR}", sys.global_cpu_usage());
 
 
     //Disks
-    println!("{TAB}{GREEN}==> Disks <=={CLR}");
+    println!("{logo1}{GREEN}==> Disks <=={CLR}");
     let disks = Disks::new_with_refreshed_list();
     for disk in &disks {
         let disk_used: String;
@@ -72,7 +77,7 @@ fn main() {
             disk_t /= 1000.0;
             disk_total = format!("{disk_t} GB");
         }
-        println!("{TAB}{GREEN}{:?}:{CLR} {} / {}", disk.name(), disk_used, disk_total);
+        println!("{logo1}{GREEN}{:?}:{CLR} {} / {}", disk.mount_point(), disk_used, disk_total);
     }
 
 }
